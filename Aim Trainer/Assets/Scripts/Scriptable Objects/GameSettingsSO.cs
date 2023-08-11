8f709225c9cb446744f7a57e53751f03713b572e
Marco <markcostah@gmail.com>
2023-08-11 21:59:39 +0100

    fix

--- Aim Trainer/Assets/Scripts/Scriptable Objects/GameSettingsSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="ScriptableObjects/GameSettingsSO")]
-public class GameSettignsSO : ScriptableObject{
-    public Gamemode chosenGamemode;
-    public ECrosshairTypes currentCrosshair;
-}
--- Aim Trainer/Assets/Scripts/UI/MainMenuUI.cs
+++ Aim Trainer/Assets/Scripts/UI/MainMenuUI.cs
@@ -10,23 +10,18 @@ public class MainMenuController : MonoBehaviour {
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
 
-    [SerializeField] private GameSettignsSO gameSettings; //USAR PLAYER PREFS
-
     [SerializeField] private GameObject mainMenuUI;
     [SerializeField] private GameObject selectGamemodeUI;
 
-    private const string FLICKING_GAMEMODE_SCENE = "Flicking";
-    private const string TRACKING_GAMEMODE_SCENE = "Tracking";
-
     private void Awake() {
         flickingGamemodeButton.onClick.AddListener(() => {
-            SceneManager.LoadScene(FLICKING_GAMEMODE_SCENE);
-            gameSettings.chosenGamemode = Gamemode.FLICKING;
+            SceneManager.LoadScene(GamemodeScenes.Flicking.ToString());
+            SaveManager.SaveChosenGamemode(Gamemode.FLICKING);
         });
 
         trackingGamemodeButton.onClick.AddListener(() => {
-            SceneManager.LoadScene(TRACKING_GAMEMODE_SCENE);
-            gameSettings.chosenGamemode = Gamemode.TRACKING;
+            SceneManager.LoadScene(GamemodeScenes.Tracking.ToString());
+            SaveManager.SaveChosenGamemode(Gamemode.TRACKING);
         });
 
         playButton.onClick.AddListener(() => {
